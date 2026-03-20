c4336e0bcdc6f2f2abaaad950ed39647ee940fc0
Karilys <karilys.learn@gmail.com>
2026-03-20 21:42:44 +0000

    Added a more verbose error message

--- SunCastPy/src/SunCastPy/utils.py
+++ SunCastPy/src/SunCastPy/utils.py
@@ -6,4 +6,6 @@ def get_request(url: str) -> dict:
     if response.status_code == 200:
         return response.json()
     else:
-        raise ValueError(f"Request failed with status code: {response.status_code}")
+        raise ValueError(
+            f"Request failed with status code: {response.status_code} {response.text}"
+        )
