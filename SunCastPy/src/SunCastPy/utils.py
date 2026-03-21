a19ce02629d94aea395d53f9069dedc88276e616
Karilys <karilys.learn@gmail.com>
2026-03-21 18:33:11 +0000

    Added docstring and replaced error

--- SunCastPy/src/SunCastPy/utils.py
+++ SunCastPy/src/SunCastPy/utils.py
@@ -1,11 +1,22 @@
+"""Standardize procedures and have a consistent error message."""
+
 import requests
 
 
 def get_request(url: str) -> dict:
+    """Run requests.get and return the json output
+
+    Args:
+        url (str): URL for the API target
+
+    Raises:
+        HTTPError: Request failed
+
+    Returns:
+        dict: Response of the API call
+    """
+
     response = requests.get(url=url)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        raise ValueError(
-            f"Request failed with status code: {response.status_code} {response.text}"
-        )
+    response.raise_for_status()
+
+    return response.json()
