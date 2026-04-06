62c78162caa55bd328515c6320e1794c4307015c
Karilys <karilys.learn@gmail.com>
2026-04-06 20:35:06 +0000

    moved functions from main to utils

--- SunCastPy/src/SunCastPy/main.py
+++ SunCastPy/src/SunCastPy/main.py
@@ -3,50 +3,11 @@ import logging
 from SunCastPy.Forecast.Base_Forecast import Forecast
 from SunCastPy.Forecast.NOAA_Local_Forecast import LocalWeather
 from SunCastPy.utils.cli_args import parse_args
+from SunCastPy.utils.current_weather import filter_current_weather, print_current_weather
 from SunCastPy.utils.logging_config import setup_logging
 
 setup_logging()
 logger = logging.getLogger(__name__)
-
-
-def filter_current_weather(
-    result: LocalWeather, group_by: str, limit: int
-) -> dict[str, list[Forecast]] | LocalWeather:
-    """Filter the weather by forecast or date
-
-    Args:
-        result (LocalWeather): data to group
-        group_by (str): [forecast, date]
-        limit (int): limit of days to show
-
-    Returns:
-        dict[str, list[Forecast]] | LocalWeather: Grouped data
-    """
-    match group_by:
-        case "forecast":
-            return result.group_by_forecast()
-        case "date":
-            return result.weekly().get_next_days(days=limit)
-        case _:
-            logger.warning("Did not group the data")
-            return result
-
-
-def print_current_weather(current_weather: LocalWeather | dict[str, list[Forecast]]) -> None:
-    """Use the logger to show the weather data
-
-    Args:
-        current_weather (LocalWeather | dict[str, list[Forecast]]): Data to print
-    """
-    if isinstance(current_weather, LocalWeather):
-        for forecast in current_weather.forecast:
-            logger.info(forecast)
-
-    elif isinstance(current_weather, dict):
-        for key, val in current_weather.items():
-            logging.info(key)
-            for item in val:
-                logging.info(item)
 
 
 def main(args=parse_args()):
--- SunCastPy/tests/test_utils.py
+++ SunCastPy/tests/test_utils.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import pytest
 
 # from SunCastPy.Forecast.NOAA_Local_Forecast import Forecast, LocalWeather
-from SunCastPy.utils import utils
+from SunCastPy.utils import current_weather, utils
 
 logger = logging.getLogger(__name__)
 
@@ -34,3 +34,15 @@ class Test_Utils:
     def test_format_date(self, attribute):
         assert "Wednesday 2024-12-25" == utils.format_date(attribute, dayname=True)
         assert "2024-12-25" == utils.format_date(attribute, dayname=False)
+
+    def test_coordinates(self, mock_get_request):
+        data = utils.get_current_coordinates()
+        assert data["latitude"] == "00.0000"
+        assert data["longitude"] == "-11.1111"
+
+
+class Test_Current_Weather:
+    @pytest.mark.skip(reason="Not yet implemented")
+    def test_placeholder(self, sample_data):
+        current_weather.filter_current_weather(sample_data["default"], group_by="none", limit=1)
+        assert False
