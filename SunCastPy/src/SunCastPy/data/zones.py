5d181c8dcb49facca21cd8fa14bcf4d68c679b60
Karilys <karilys.learn@gmail.com>
2026-04-12 21:47:44 +0000

    added dictionary with coordinates for each of the 78 PR municipalities

---
+++ SunCastPy/src/SunCastPy/data/zones.py
@@ -0,0 +1,314 @@
+COORDINATES = {
+    "Adjuntas": {
+        "latitude": 18.1633,
+        "longitude": -66.7224,
+    },
+    "Aguada": {
+        "latitude": 18.3794,
+        "longitude": -67.1882,
+    },
+    "Aguadilla": {
+        "latitude": 18.4274,
+        "longitude": -67.1541,
+    },
+    "Aguas Buenas": {
+        "latitude": 18.2569,
+        "longitude": -66.1029,
+    },
+    "Aibonito": {
+        "latitude": 18.1397,
+        "longitude": -66.2660,
+    },
+    "Anasco": {
+        "latitude": 18.2841,
+        "longitude": -67.1396,
+    },
+    "Arecibo": {
+        "latitude": 18.4705,
+        "longitude": -66.7218,
+    },
+    "Arroyo": {
+        "latitude": 17.9658,
+        "longitude": -66.0610,
+    },
+    "Barceloneta": {
+        "latitude": 18.4505,
+        "longitude": -66.5385,
+    },
+    "Barranquitas": {
+        "latitude": 18.1866,
+        "longitude": -66.3063,
+    },
+    "Bayamon": {
+        "latitude": 18.3986,
+        "longitude": -66.1557,
+    },
+    "Cabo Rojo": {
+        "latitude": 18.0866,
+        "longitude": -67.1457,
+    },
+    "Caguas": {
+        "latitude": 18.2341,
+        "longitude": -66.0485,
+    },
+    "Camuy": {
+        "latitude": 18.4836,
+        "longitude": -66.8449,
+    },
+    "Canovanas": {
+        "latitude": 18.3744,
+        "longitude": -65.8993,
+    },
+    "Carolina": {
+        "latitude": 18.3808,
+        "longitude": -65.9574,
+    },
+    "Catano": {
+        "latitude": 18.4413,
+        "longitude": -66.1182,
+    },
+    "Cayey": {
+        "latitude": 18.1119,
+        "longitude": -66.1660,
+    },
+    "Ceiba": {
+        "latitude": 18.2644,
+        "longitude": -65.6483,
+    },
+    "Ciales": {
+        "latitude": 18.3360,
+        "longitude": -66.4682,
+    },
+    "Cidra": {
+        "latitude": 18.1758,
+        "longitude": -66.1616,
+    },
+    "Coamo": {
+        "latitude": 18.0791,
+        "longitude": -66.3579,
+    },
+    "Comerio": {
+        "latitude": 18.2191,
+        "longitude": -66.2260,
+    },
+    "Corozal": {
+        "latitude": 18.3416,
+        "longitude": -66.3160,
+    },
+    "Culebra": {
+        "latitude": 18.3030,
+        "longitude": -65.3000,
+    },
+    "Dorado": {
+        "latitude": 18.4588,
+        "longitude": -66.2677,
+    },
+    "Fajardo": {
+        "latitude": 18.3258,
+        "longitude": -65.6524,
+    },
+    "Florida": {
+        "latitude": 18.3622,
+        "longitude": -66.5671,
+    },
+    "Guanica": {
+        "latitude": 17.9716,
+        "longitude": -66.9080,
+    },
+    "Guayama": {
+        "latitude": 17.9841,
+        "longitude": -66.1138,
+    },
+    "Guayanilla": {
+        "latitude": 18.0191,
+        "longitude": -66.7921,
+    },
+    "Guaynabo": {
+        "latitude": 18.3613,
+        "longitude": -66.1100,
+    },
+    "Gurabo": {
+        "latitude": 18.2544,
+        "longitude": -65.9729,
+    },
+    "Hatillo": {
+        "latitude": 18.4855,
+        "longitude": -66.8254,
+    },
+    "Hormigueros": {
+        "latitude": 18.1330,
+        "longitude": -67.1124,
+    },
+    "Humacao": {
+        "latitude": 18.1497,
+        "longitude": -65.8274,
+    },
+    "Isabela": {
+        "latitude": 18.5008,
+        "longitude": -67.0243,
+    },
+    "Jayuya": {
+        "latitude": 18.2186,
+        "longitude": -66.5924,
+    },
+    "Juana Diaz": {
+        "latitude": 18.0525,
+        "longitude": -66.5074,
+    },
+    "Juncos": {
+        "latitude": 18.2275,
+        "longitude": -65.9210,
+    },
+    "Lajas": {
+        "latitude": 18.0497,
+        "longitude": -67.0599,
+    },
+    "Lares": {
+        "latitude": 18.2941,
+        "longitude": -66.8766,
+    },
+    "Las Marias": {
+        "latitude": 18.2511,
+        "longitude": -66.9921,
+    },
+    "Las Piedras": {
+        "latitude": 18.1836,
+        "longitude": -65.8674,
+    },
+    "Loiza": {
+        "latitude": 18.4313,
+        "longitude": -65.8802,
+    },
+    "Luquillo": {
+        "latitude": 18.3725,
+        "longitude": -65.7166,
+    },
+    "Manati": {
+        "latitude": 18.4275,
+        "longitude": -66.4921,
+    },
+    "Maricao": {
+        "latitude": 18.1800,
+        "longitude": -66.9791,
+    },
+    "Maunabo": {
+        "latitude": 18.0075,
+        "longitude": -65.8993,
+    },
+    "Mayaguez": {
+        "latitude": 18.2011,
+        "longitude": -67.1396,
+    },
+    "Moca": {
+        "latitude": 18.3966,
+        "longitude": -67.1132,
+    },
+    "Morovis": {
+        "latitude": 18.3258,
+        "longitude": -66.4060,
+    },
+    "Naguabo": {
+        "latitude": 18.2119,
+        "longitude": -65.7349,
+    },
+    "Naranjito": {
+        "latitude": 18.3005,
+        "longitude": -66.2449,
+    },
+    "Orocovis": {
+        "latitude": 18.2266,
+        "longitude": -66.3916,
+    },
+    "Patillas": {
+        "latitude": 18.0036,
+        "longitude": -66.0152,
+    },
+    "Penuelas": {
+        "latitude": 18.0550,
+        "longitude": -66.7210,
+    },
+    "Ponce": {
+        "latitude": 18.0111,
+        "longitude": -66.6141,
+    },
+    "Quebradillas": {
+        "latitude": 18.4736,
+        "longitude": -66.9385,
+    },
+    "Rincon": {
+        "latitude": 18.3402,
+        "longitude": -67.2499,
+    },
+    "Rio Grande": {
+        "latitude": 18.3802,
+        "longitude": -65.8313,
+    },
+    "Sabana Grande": {
+        "latitude": 18.0775,
+        "longitude": -66.9605,
+    },
+    "Salinas": {
+        "latitude": 17.9775,
+        "longitude": -66.2979,
+    },
+    "San German": {
+        "latitude": 18.0811,
+        "longitude": -67.0458,
+    },
+    "San Juan": {
+        "latitude": 18.4655,
+        "longitude": -66.1057,
+    },
+    "San Lorenzo": {
+        "latitude": 18.1883,
+        "longitude": -65.9766,
+    },
+    "San Sebastian": {
+        "latitude": 18.3366,
+        "longitude": -66.9902,
+    },
+    "Santa Isabel": {
+        "latitude": 17.9661,
+        "longitude": -66.4049,
+    },
+    "Toa Alta": {
+        "latitude": 18.3883,
+        "longitude": -66.2482,
+    },
+    "Toa Baja": {
+        "latitude": 18.4444,
+        "longitude": -66.2543,
+    },
+    "Trujillo Alto": {
+        "latitude": 18.3547,
+        "longitude": -66.0074,
+    },
+    "Utuado": {
+        "latitude": 18.2655,
+        "longitude": -66.7004,
+    },
+    "Vega Alta": {
+        "latitude": 18.4122,
+        "longitude": -66.3313,
+    },
+    "Vega Baja": {
+        "latitude": 18.4444,
+        "longitude": -66.3877,
+    },
+    "Vieques": {
+        "latitude": 18.1263,
+        "longitude": -65.4401,
+    },
+    "Villalba": {
+        "latitude": 18.1272,
+        "longitude": -66.4921,
+    },
+    "Yabucoa": {
+        "latitude": 18.0500,
+        "longitude": -65.8793,
+    },
+    "Yauco": {
+        "latitude": 18.0349,
+        "longitude": -66.8499,
+    },
+}
