414f38ad7331945b50fefdc154513fccf8b92719
Karilys <karilys.learn@gmail.com>
2026-04-17 21:31:02 +0000

    Added main function and logger

--- SunCastPy/src/SunCastPy/utils/sju_zones.py
+++ SunCastPy/src/SunCastPy/utils/sju_zones.py
@@ -1,15 +1,24 @@
 import importlib.util
 import json
+import logging
 from pathlib import Path
 
 from SunCastPy.data import zones
-from SunCastPy.utils.utils import get_api_details, get_hourly_forecast_url
+from SunCastPy.utils.logging_config import setup_logging
+from SunCastPy.utils.utils import (
+    get_api_details,
+    get_hourly_forecast_url,
+    get_hourly_forecast_zone_url,
+)
 
 
 def get_all_zones() -> dict[str, str]:
     data: dict = zones.COORDINATES.copy()
     for city, kwargs in zones.COORDINATES.items():
-        data[city]["url"] = get_hourly_forecast_url(get_api_details(**kwargs))
+        logging.info(f"Getting values for {city}")
+        details = get_api_details(**kwargs)
+        data[city]["url"] = get_hourly_forecast_url(details)
+        data[city]["forecastZone"] = get_hourly_forecast_zone_url(details)
     return data
 
 
@@ -27,9 +36,15 @@ def get_data_path() -> Path:
 def export_zones_url(data, data_file) -> None:
     with open(data_file, "w", encoding="utf-8") as file:
         json.dump(data, file, indent=4)
+    logging.info("File was successfully updated.")
 
 
-if __name__ == "__main__":
+def main():
+    setup_logging()
     data_path = get_data_path().joinpath("zones_url.json")
     data = get_all_zones()
     export_zones_url(data=data, data_file=data_path)
+
+
+if __name__ == "__main__":
+    main()
