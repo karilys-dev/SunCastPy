5e05e2f85b5da943dfea18153afd7ad0ed0d925c
Karilys <karilys.learn@gmail.com>
2026-04-22 21:14:45 +0000

    moved export json

--- SunCastPy/src/SunCastPy/utils/export_file.py
+++ SunCastPy/src/SunCastPy/utils/export_file.py
@@ -1,3 +1,5 @@
+import json
+import logging
 from pathlib import Path
 
 
@@ -6,3 +8,9 @@ def create_htlm(data: str, output_dir: Path) -> None:
     output_dir.mkdir(exist_ok=True)
 
     (output_dir / "index.html").write_text(data)
+
+
+def export_json(data, data_file) -> None:
+    with open(data_file, "w", encoding="utf-8") as file:
+        json.dump(data, file, indent=4)  # noqa: F821
+    logging.info("File was successfully updated.")
