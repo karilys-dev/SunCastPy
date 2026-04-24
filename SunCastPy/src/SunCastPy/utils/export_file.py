4daa5d4b6fab0883647a1aa0e775d779af5fffcd
Karilys <karilys.learn@gmail.com>
2026-04-24 20:23:47 +0000

    added parameter to allow reuse

--- SunCastPy/src/SunCastPy/utils/export_file.py
+++ SunCastPy/src/SunCastPy/utils/export_file.py
@@ -3,11 +3,12 @@ import logging
 from pathlib import Path
 
 
-def create_htlm(data: str, output_dir: Path) -> None:
-
+def create_htlm(data: str, output_dir: Path, name: str) -> None:
+    if isinstance(output_dir, str):
+        output_dir = Path(output_dir)
     output_dir.mkdir(exist_ok=True)
 
-    (output_dir / "index.html").write_text(data)
+    (output_dir / name).write_text(data)
 
 
 def export_json(data, data_file) -> None:
