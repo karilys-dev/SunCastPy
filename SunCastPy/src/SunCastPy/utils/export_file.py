456a062641e3aec16a25d6db9d896aa9ba8ed8a8
Karilys <karilys.learn@gmail.com>
2026-04-27 19:10:48 +0000

    make parent dirs true

--- SunCastPy/src/SunCastPy/utils/export_file.py
+++ SunCastPy/src/SunCastPy/utils/export_file.py
@@ -6,7 +6,7 @@ from pathlib import Path
 def create_html(data: str, output_dir: Path, name: str) -> None:
     if isinstance(output_dir, str):
         output_dir = Path(output_dir)
-    output_dir.mkdir(exist_ok=True)
+    output_dir.mkdir(exist_ok=True, parents=True)
 
     (output_dir / name).write_text(data)
 
