165bca0d56bc9594767fe585a2aa3c38a10394f9
Karilys <karilys.learn@gmail.com>
2026-05-09 15:58:19 +0000

    add robot framework

--- scripts/docker-postCreateCommand.sh
+++ scripts/docker-postCreateCommand.sh
@@ -6,7 +6,7 @@ echo 'export PATH=$HOME/.venv/bin:$HOME/.local/bin:$PATH' >> ~/.bashrc
 python -m venv "$HOME/.venv"
 
 "$HOME/.venv/bin/pip" install --upgrade pip
-"$HOME/.venv/bin/pip" install -e SunCastPy[tests,dev]
+"$HOME/.venv/bin/pip" install -e SunCastPy[tests,dev,test-black]
 
 git config --global user.email ${USER_EMAIL}
 git config --global user.name ${USER_NAME}
