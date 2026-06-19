e1dcb18dc36593bdf28740f83a81cdc06cc09f34
Karilys <karilys.learn@gmail.com>
2026-06-19 20:29:02 +0000

    separated install and run

--- scripts/ci-cd/run-robot.sh
+++ scripts/ci-cd/run-robot.sh
@@ -1,20 +1,44 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-ARTIFACTS=${OUTPUT_DIR}/robot
-if [ -d "${ARTIFACTS}" ]; then
-  rm -rf ${ARTIFACTS}
-fi
+install(){
+  if command -v robot &> /dev/null; then
+      robot --version || true
+      echo "Success: robot is installed and ready."
+  else
+      echo "Installing: robot-framework."
+      DIR="$( realpath $(dirname "${BASH_SOURCE[0]}")/../../ )"
+      pip install ${DIR}/SunCastPy[test-robot]
+  fi
+}
 
-if command -v robot &> /dev/null; then
-    echo "Success: robot is installed and ready."
-else
-    echo "Installing: robot-framework."
-    DIR="$( realpath $(dirname "${BASH_SOURCE[0]}")/../../ )"
-    pip install ${DIR}/SunCastPy[test-robot]
-fi
+run(){
+  ARTIFACTS=${OUTPUT_DIR}/robot
+  if [ -d "${ARTIFACTS}" ]; then
+    rm -rf ${ARTIFACTS}
+  fi
 
-robot \
-    --outputdir=${ARTIFACTS} \
-    --loglevel=TRACE \
-    . 
+
+  robot \
+      --outputdir=${ARTIFACTS} \
+      --loglevel=TRACE \
+      . 
+}
+
+
+usage() {
+    echo "Usage: $(basename "$0") [-h help] [-r robot] [-i install]" >&2
+}
+
+while getopts ":irh" opt; do
+    case $opt in
+        i) install ;;
+        r) run ;;
+        h) usage ;;
+        \?)
+          echo "Invalid option: -$OPTARG" >&2
+          exit 1
+          ;;
+    esac
+done
+shift $((OPTIND - 1))
