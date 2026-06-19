3a9a8a006a73c7b31e5a409f71147b0e571bdd8a
Karilys <karilys.learn@gmail.com>
2026-06-19 19:34:53 +0000

    added section to verify if robot is installed and if its not it installs it

--- scripts/ci-cd/run-robot.sh
+++ scripts/ci-cd/run-robot.sh
@@ -6,6 +6,14 @@ if [ -d "${ARTIFACTS}" ]; then
   rm -rf ${ARTIFACTS}
 fi
 
+if command -v robot &> /dev/null; then
+    echo "Success: robot is installed and ready."
+else
+    echo "Installing: robot-framework."
+    DIR="$( realpath $(dirname "${BASH_SOURCE[0]}")/../../ )"
+    pip install ${DIR}/SunCastPy[test-robot]
+fi
+
 robot \
     --outputdir=${ARTIFACTS} \
     --loglevel=TRACE \
