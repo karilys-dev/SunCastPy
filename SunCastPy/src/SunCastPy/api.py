f3df4c0cde80b834762064f10195b67b9d94b5cf
Karilys <karilys.learn@gmail.com>
2026-07-24 17:07:47 +0000

    Added api response to view as html instead of json. Note it only works for city

--- SunCastPy/src/SunCastPy/api.py
+++ SunCastPy/src/SunCastPy/api.py
@@ -1,8 +1,10 @@
 """Module used for forecast API"""
 
 from fastapi import FastAPI, HTTPException
+from fastapi.responses import HTMLResponse
 
-from SunCastPy.report.city import get_city_forecast as city_report
+from SunCastPy.report.city import get_city_forecast as city_forecast
+from SunCastPy.report.city import main as city_report
 from SunCastPy.report.zone import get_forecast_all_cities_in_zone as zone_report
 
 app = FastAPI(title="SunCast API", version="1.0.0")
@@ -20,7 +22,7 @@ def forecast_city(
 
     try:
         return (
-            city_report(
+            city_forecast(
                 {
                     "city": city,
                     "flatten": flatten,
@@ -47,6 +49,34 @@ def forecast_zone(
 
     try:
         return zone_report(zone_name, flatten, limit)
+
+    except Exception as exc:
+        raise HTTPException(status_code=404, detail=str(exc)) from exc
+
+
+@app.get("/report/{city}", response_class=HTMLResponse)
+def report_city(
+    city: str,
+    flatten: bool = True,
+    limit: int = 1,
+    group_by: str | None = "",
+):
+    """
+    Returns the html report for the requested city.
+    """
+
+    try:
+        return HTMLResponse(
+            content=city_report(
+                {
+                    "city": city,
+                    "flatten": flatten,
+                    "limit": limit,
+                },
+                group_by=group_by,
+                html_report=True,
+            )
+        )
 
     except Exception as exc:
         raise HTTPException(status_code=404, detail=str(exc)) from exc
